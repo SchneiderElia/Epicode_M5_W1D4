f7b1b7254724f1c67fe4401d544f2a31ccc2e4d1
Elia Schneider <elyssch@gmail.com>
2025-01-21 20:15:54 +0100

    renormaliza file LF to CRLF + add bootstrap and make first box to check

--- epibooks/src/App.js
+++ epibooks/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='container'>
+        <div className='row'>
+          <div className='col-6 bg-succes'>
+            hello 1
+          </div>
+          <div className='col-6 bg-primary'>
+            hello 2
+          </div>
+        </div>
+      </div>
+     {/*  <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +28,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
