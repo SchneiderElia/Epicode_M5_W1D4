dc95af49da70a317056630e04197b63b413b7ede
Elia Schneider <elyssch@gmail.com>
2025-01-25 22:21:49 +0100

    navBar rendered

--- epibooks/src/App.js
+++ epibooks/src/App.js
@@ -1,35 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import MyNav from './MyNav';
+
+
 function App() {
   return (
-    <div className="App">
-      <div className='container'>
-        <div className='row'>
-          <div className='col-6 bg-succes'>
-            hello 1
-          </div>
-          <div className='col-6 bg-primary'>
-            hello 2
-          </div>
-        </div>
-      </div>
-     {/*  <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <body className='App'>
+      <headre>
+        <MyNav/>
+      </headre>
+
+    </body>
+      
   );
 }
 
