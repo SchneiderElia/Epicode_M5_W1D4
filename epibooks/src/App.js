2e62c12a76edc11ebbe2c74558aa920a8c2933ce
Elia Schneider <elyssch@gmail.com>
2025-01-25 23:22:29 +0100

    alert rendered

--- epibooks/src/App.js
+++ epibooks/src/App.js
@@ -4,23 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import MyNav from './MyNav'
 import MyFooter from './MyFooter'
+import Alert from './Alert'
+
+
+
+
 
 
 
 function App() {
   return (
-    <body className='App'>
-      <headre>
-        <MyNav/>
-      </headre>
-    <div className='App-content'>
+    <div className='App'>
 
-    </div>
+      <MyNav />
+
+      <div className='App-content'>
+        <div className='container mt-5'>
+          <Alert />
+        </div>
+
+      </div>
       <footer>
-        <MyFooter/>
+        <MyFooter />
       </footer>
-    </body>
-      
+    </div>
+
   );
 }
 
